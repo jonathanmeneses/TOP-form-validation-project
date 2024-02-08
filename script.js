8878b334ac4d81fc8863294d9ca8c3cfb1e40f9f
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-02-07 23:42:58 -0600

    start on js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Add event listener for password fields
+
+let confirmPasswordField = document.querySelector('#password_confirm')
+let passwordField = document.querySelector('#password')
+
+passwordField.addEventListener('change', checkPassword())
+
+
+// checkpassword function
+
+function checkPassword() {
+    pass
+}
+
+// style update functions
+
+function addRequiredFormatting() {
+    pass
+}
